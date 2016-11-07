5d936e8984dd1102aa906b11b7b81d5cd1b9eac4
Mikkel Oscar Lyderik Larsen <m@moscar.net>
2016-11-07 09:07:41 +0100

    Test for PointerMotion cb

--- compositor/compositor_test.go
+++ compositor/compositor_test.go
@@ -3,9 +3,18 @@ package compositor
 import (
 	"testing"
 
+	"github.com/mikkeloscar/flise/backend"
 	"github.com/mikkeloscar/flise/config"
+	wlc "github.com/mikkeloscar/go-wlc"
 	xkb "github.com/mikkeloscar/go-xkbcommon"
 )
+
+func TestPointerMotion(t *testing.T) {
+	c := New(nil, backend.Mock{}, nil)
+	if !c.PointerMotion(0, 0, &wlc.PointZero) {
+		t.Errorf("expected pointer motion to succeed")
+	}
+}
 
 // Test validBinding function.
 func TestValidBinding(t *testing.T) {
